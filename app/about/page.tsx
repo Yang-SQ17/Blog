52fc6608eefc4b4f01529758e69f0f3f133d91f4
shiqi7777 <yang824yang@gmail.com>
2026-09-10 17:54:36 +0800

    背景图修改

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -45,6 +45,7 @@ export default async function AboutPage() {
     contentHtml = processedContent.toString();
   } catch (e) {}
 
+
   // Get activities from database
   const allActivities: { id: string; type: string; title: string; date: string; url: string }[] = [];
 
